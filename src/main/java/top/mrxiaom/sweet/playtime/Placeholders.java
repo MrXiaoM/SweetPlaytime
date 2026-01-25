201943d275c4b5ee1ca99709b8e4fd37c981dd10
MrXiaoM <coolxiaom95@gmail.com>
2026-01-25 13:54:00 +0800

    move query parser to a single class

--- src/main/java/top/mrxiaom/sweet/playtime/Placeholders.java
+++ src/main/java/top/mrxiaom/sweet/playtime/Placeholders.java
@@ -1,17 +1,15 @@
 package top.mrxiaom.sweet.playtime;
 
+import com.google.common.collect.Lists;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import top.mrxiaom.pluginbase.utils.Util;
 import top.mrxiaom.pluginbase.utils.depend.PlaceholdersExpansion;
+import top.mrxiaom.sweet.playtime.config.Query;
 import top.mrxiaom.sweet.playtime.database.PlaytimeDatabase;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Placeholders extends PlaceholdersExpansion<SweetPlaytime> {
     protected Placeholders(SweetPlaytime plugin) {
@@ -27,49 +25,20 @@ public class Placeholders extends PlaceholdersExpansion<SweetPlaytime> {
     public @Nullable String onPlaceholderRequest(Player player, @NotNull String params) {
         if (params.equalsIgnoreCase("query") || params.startsWith("query_")) {
             int beginIndex = params.indexOf('_');
-            String tag = null;
-            LocalDate startDate = null, endDate = null;
+            Query query;
             if (beginIndex > 2) {
                 String[] args = params.substring(beginIndex + 1).split(",");
-                for (String arg : args) {
-                    String str = arg.trim();
-                    if (str.equalsIgnoreCase("today")) {
-                        startDate = LocalDate.now();
-                        continue;
-                    }
-                    DayOfWeek dayOfWeek = Util.valueOrNull(DayOfWeek.class, str);
-                    if (dayOfWeek != null) {
-                        LocalDate now = LocalDate.now();
-                        LocalDate monday = now.minusDays(now.getDayOfWeek().getValue() - 1);
-                        startDate = monday.plusDays(dayOfWeek.getValue() - 1);
-                    }
-                    if (!str.contains("=")) continue;
-                    int i = str.indexOf('=');
-                    String key = str.substring(0, i);
-                    String value = str.substring(i + 1);
-                    if (key.equals("tag")) {
-                        tag = value;
-                        continue;
-                    }
-                    if (key.equals("start")) {
-                        try {
-                            startDate = LocalDate.parse(value, DateTimeFormatter.ISO_LOCAL_DATE);
-                        } catch (DateTimeParseException e) {
-                            return "WRONG_START_DATE_FORMAT";
-                        }
-                    }
-                    if (key.equals("end")) {
-                        try {
-                            endDate = LocalDate.parse(value, DateTimeFormatter.ISO_LOCAL_DATE);
-                        } catch (DateTimeParseException e) {
-                            return "WRONG_END_DATE_FORMAT";
-                        }
-                    }
+                AtomicReference<String> error = new AtomicReference<>("UNKNOWN_ERROR");
+                query = Query.parse(Lists.newArrayList(args), error);
+                if (query == null) {
+                    return error.get();
                 }
+            } else {
+                query = Query.ALL;
             }
             PlaytimeDatabase db = plugin.getPlaytimeDatabase();
             UUID uuid = player.getUniqueId();
-            Long fromDb = db.collectPlaytimeWithCache(uuid, tag, startDate, endDate);
+            Long fromDb = query.collectPlaytimeWithCache(db, uuid);
             if (fromDb == null) {
                 return "DATABASE_ERROR";
             }
---
+++ src/main/java/top/mrxiaom/sweet/playtime/config/Query.java
@@ -0,0 +1,88 @@
+package top.mrxiaom.sweet.playtime.config;
+
+import org.jetbrains.annotations.Nullable;
+import top.mrxiaom.pluginbase.utils.Util;
+import top.mrxiaom.sweet.playtime.database.PlaytimeDatabase;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Query {
+    public static final Query ALL = new Query(null, null, null);
+    private final @Nullable String tag;
+    private final @Nullable LocalDate startDate;
+    private final @Nullable LocalDate endDate;
+    public Query(@Nullable String tag, @Nullable LocalDate startDate, @Nullable LocalDate endDate) {
+        this.tag = tag;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public @Nullable String getTag() {
+        return tag;
+    }
+
+    public @Nullable LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public @Nullable LocalDate getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @see PlaytimeDatabase#collectPlaytimeWithCache(UUID, String, LocalDate, LocalDate)
+     */
+    public @Nullable Long collectPlaytimeWithCache(PlaytimeDatabase db, UUID playerUUID) {
+        return db.collectPlaytimeWithCache(playerUUID, tag, startDate, endDate);
+    }
+
+    @Nullable
+    public static Query parse(List<String> args, @Nullable AtomicReference<String> error) {
+        String tag = null;
+        LocalDate startDate = null, endDate = null;
+        for (String arg : args) {
+            String str = arg.trim();
+            if (str.equalsIgnoreCase("today")) {
+                startDate = LocalDate.now();
+                continue;
+            }
+            DayOfWeek dayOfWeek = Util.valueOrNull(DayOfWeek.class, str);
+            if (dayOfWeek != null) {
+                LocalDate now = LocalDate.now();
+                LocalDate monday = now.minusDays(now.getDayOfWeek().getValue() - 1);
+                startDate = monday.plusDays(dayOfWeek.getValue() - 1);
+            }
+            if (!str.contains("=")) continue;
+            int i = str.indexOf('=');
+            String key = str.substring(0, i);
+            String value = str.substring(i + 1);
+            if (key.equals("tag")) {
+                tag = value;
+                continue;
+            }
+            if (key.equals("start")) {
+                try {
+                    startDate = LocalDate.parse(value, DateTimeFormatter.ISO_LOCAL_DATE);
+                } catch (DateTimeParseException e) {
+                    if (error != null) error.set("WRONG_START_DATE_FORMAT");
+                    return null;
+                }
+            }
+            if (key.equals("end")) {
+                try {
+                    endDate = LocalDate.parse(value, DateTimeFormatter.ISO_LOCAL_DATE);
+                } catch (DateTimeParseException e) {
+                    if (error != null) error.set("WRONG_END_DATE_FORMAT");
+                    return null;
+                }
+            }
+        }
+        return new Query(tag, startDate, endDate);
+    }
+}
